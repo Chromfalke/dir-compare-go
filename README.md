c0e91a326d66619ac7203a4a46f167f596a84b8a
Chromfalke <47539373+Chromfalke@users.noreply.github.com>
2024-07-12 20:28:43 +0200

    Initial working version

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # dir-compare-go
-A simple small tool to compare the sizes of files in two directories
+A simple small tool to compare the sizes of files in two directories.
+Outputs a table using [lipgloss](https://github.com/charmbracelet/lipgloss) with old and new filesizes as well as a comparison of the total difference.
+The filesizes are displayed in the best matching unit of Bytes, Kibibytes, Mibibytes, etc.
+
+## Planned expansion
+Eventually the tool is supposed to include subdirectories and their contents.
+
+## Example
+```
+dir-compare /path/to/source /path/to/dest
+
+┌───────────┬────────────┬───────────┬──────────────────┐
+│ File      │ Orig. Size │ New Size  │ Saved Space      │
+├───────────┼────────────┼───────────┼──────────────────┤
+│ SomeFile1 │ 127.1 MiB  │ 18.0 MiB  │ 109.1 MiB        │
+│ SomeFile2 │ 135.7 MiB  │ 149.2 MiB │ -13.5 MiB        │
+│ Total     │ 262.8 MiB  │ 167.2 MiB │ 95.6 MiB (1.6 %) │
+└───────────┴────────────┴───────────┴──────────────────┘
+```
---
+++ dir-compare.go
@@ -0,0 +1,164 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"math"
+	"os"
+	"sort"
+
+	"github.com/charmbracelet/lipgloss"
+	"github.com/charmbracelet/lipgloss/table"
+)
+
+// struct to hold info for a file
+type File struct {
+	Name         string
+	OriginalSize int64
+	NewSize      int64
+	Difference   int64
+}
+
+// slice of files with custom sorter
+type BySize []File
+
+func (files BySize) Len() int {
+	return len(files)
+}
+
+func (files BySize) Swap(i int, j int) {
+	files[i], files[j] = files[j], files[i]
+}
+
+func (files BySize) Less(i int, j int) bool {
+	return files[i].OriginalSize < files[j].OriginalSize
+}
+
+func main() {
+	args := os.Args
+	if len(args) < 3 {
+		log.Fatal("not enough arguments suplied")
+	}
+
+	// check if src and dest exist
+	info, err := os.Stat(args[1])
+	if err != nil {
+		if os.IsNotExist(err) {
+			log.Fatalf("the directory %s does not exist", args[1])
+		}
+	}
+	if !info.IsDir() {
+		log.Fatal("first argument is not a directory")
+	}
+
+	info, err = os.Stat(args[2])
+	if err != nil {
+		if os.IsNotExist(err) {
+			log.Fatalf("the directory %s does not exist", args[1])
+		}
+	}
+	if !info.IsDir() {
+		log.Fatal("second argument is not a directory")
+	}
+
+	// read files in src
+	filenames, err := os.ReadDir(args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fileDict := make(map[string]File)
+	for _, file := range filenames {
+		info, err := file.Info()
+		if err != nil {
+			log.Fatal(err)
+		}
+		if info.Mode().IsRegular() {
+			fileDict[info.Name()] = File{
+				Name:         info.Name(),
+				OriginalSize: info.Size(),
+				NewSize:      0,
+				Difference:   0.0,
+			}
+		}
+	}
+
+	// get sizes for files in dest if they exist
+	filenames, err = os.ReadDir(args[2])
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, file := range filenames {
+		info, err := file.Info()
+		if err != nil {
+			log.Fatal(err)
+		}
+		if info.Mode().IsRegular() {
+			if _, exists := fileDict[info.Name()]; exists {
+				file := fileDict[info.Name()]
+				file.NewSize = info.Size()
+				file.Difference = file.OriginalSize - file.NewSize
+				fileDict[info.Name()] = file
+			}
+		}
+	}
+
+	// sort files by original size
+	files := []File{}
+	for file := range fileDict {
+		files = append(files, fileDict[file])
+	}
+	sort.Sort(BySize(files))
+
+	// build and display table
+	filesTable := table.New().
+		Border(lipgloss.NormalBorder()).
+		BorderStyle(lipgloss.NewStyle().Foreground(lipgloss.Color("#2DE224"))).
+		Headers("File", "Orig. Size", "New Size", "Saved Space").
+		StyleFunc(func(row int, col int) lipgloss.Style {
+			return lipgloss.NewStyle().Padding(0, 1)
+		})
+
+	var totalOrigSize int64
+	var totalNewSize int64
+	for _, file := range files {
+		if file.NewSize != 0.0 {
+			filesTable.Row(file.Name, convertBytes(file.OriginalSize), convertBytes(file.NewSize), convertBytes(file.Difference))
+			totalOrigSize += file.OriginalSize
+			totalNewSize += file.NewSize
+		}
+	}
+	totalDifference := totalOrigSize - totalNewSize
+	totalDifferencePercent := float64(totalOrigSize) / float64(totalNewSize)
+	filesTable.Row("Total", convertBytes(totalOrigSize), convertBytes(totalNewSize), convertBytes(totalDifference)+fmt.Sprintf(" (%.1f %%)", totalDifferencePercent))
+
+	fmt.Println(filesTable.Render())
+}
+
+// converts the given number of bytes to the closest representation as KiB, MiB, GiB, ...
+func convertBytes(numBytes int64) string {
+	representation := float64(numBytes)
+	counter := 0
+	for math.Abs(representation) > 1024.0 {
+		representation /= 1024.0
+		counter += 1
+	}
+	switch counter {
+	case 0:
+		return fmt.Sprintf("%.1f B", representation)
+	case 1:
+		return fmt.Sprintf("%.1f KiB", representation)
+	case 2:
+		return fmt.Sprintf("%.1f MiB", representation)
+	case 3:
+		return fmt.Sprintf("%.1f GiB", representation)
+	case 4:
+		return fmt.Sprintf("%.1f TiB", representation)
+	case 5:
+		return fmt.Sprintf("%.1f PiB", representation)
+	case 6:
+		return fmt.Sprintf("%.1f EiB", representation)
+	}
+
+	return "N/A"
+}
